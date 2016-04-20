faec9e82b1f40f9c1658498ff9efca020e50819f
atekin-tgm <atekin@student.tgm.ac.at>
2016-04-20 09:38:58 +0200

    Added test-methods to MockitoTest.java

--- src/showcase/MockitoTest.java
+++ src/showcase/MockitoTest.java
@@ -65,12 +65,41 @@ public class MockitoTest {
 		 when(mockedList.get(anyInt())).thenReturn("element");
 
 		 //stubbing using custom matcher (let's say isValid() returns your own matcher implementation):
-		 when(mockedList.contains(argThat(isValid()))).thenReturn("element");
+		 // when(mockedList.contains(argThat(isValid()))).thenReturn("element");
 
 		 //following prints "element"
 		 System.out.println(mockedList.get(999));
 
 		 //you can also verify using an argument matcher
 		 verify(mockedList).get(anyInt());
+	}
+	
+	@Test
+	public void testNumberOfInvocations() {
+		//using mock
+		 mockedList.add("once");
+
+		 mockedList.add("twice");
+		 mockedList.add("twice");
+
+		 mockedList.add("three times");
+		 mockedList.add("three times");
+		 mockedList.add("three times");
+
+		 //following two verifications work exactly the same - times(1) is used by default
+		 verify(mockedList).add("once");
+		 verify(mockedList, times(1)).add("once");
+
+		 //exact number of invocations verification
+		 verify(mockedList, times(2)).add("twice");
+		 verify(mockedList, times(3)).add("three times");
+
+		 //verification using never(). never() is an alias to times(0)
+		 verify(mockedList, never()).add("never happened");
+
+		 //verification using atLeast()/atMost()
+		 verify(mockedList, atLeastOnce()).add("three times");
+		 verify(mockedList, atLeast(2)).add("twice");
+		 verify(mockedList, atMost(5)).add("three times");
 	}
 }
