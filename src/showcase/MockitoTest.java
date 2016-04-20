14420acb8f8aed7231ca1428f85dfb39d4fab350
atekin-tgm <atekin@student.tgm.ac.at>
2016-04-20 09:26:19 +0200

    Updated the MockitoTest.java file

--- src/showcase/MockitoTest.java
+++ src/showcase/MockitoTest.java
@@ -1,7 +1,7 @@
 package showcase;
 
 import static org.mockito.Mockito.*;
-
+import static org.mockito.Matchers.*;
 import java.util.LinkedList;
 import java.util.List;
 import org.junit.Test;
@@ -13,6 +13,8 @@ import org.junit.Test;
 
 public class MockitoTest {
 
+	LinkedList mockedList;
+	
 	@Test
 	public void testBehaviour() {
 		//Let's import Mockito statically so that the code looks clearer
@@ -30,10 +32,10 @@ public class MockitoTest {
 		 verify(mockedList).clear();
 	}
 	
-	@Test
+	@Test(expected=RuntimeException.class)
 	public void testStubbing() {
 		//You can mock concrete classes, not just interfaces
-		 LinkedList mockedList = mock(LinkedList.class);
+		mockedList = mock(LinkedList.class);
 
 		 //stubbing
 		 when(mockedList.get(0)).thenReturn("first");
@@ -52,6 +54,23 @@ public class MockitoTest {
 		 //If your code cares what get(0) returns, then something else breaks (often even before verify() gets executed).
 		 //If your code doesn't care what get(0) returns, then it should not be stubbed. Not convinced? See here.
 		 verify(mockedList).get(0);
+	}
+	
+	@Test
+	public void testArgumentMather() {
+		
+		mockedList = mock(LinkedList.class);
+		
+		//stubbing using built-in anyInt() argument matcher
+		 when(mockedList.get(anyInt())).thenReturn("element");
 
+		 //stubbing using custom matcher (let's say isValid() returns your own matcher implementation):
+		 when(mockedList.contains(argThat(isValid()))).thenReturn("element");
+
+		 //following prints "element"
+		 System.out.println(mockedList.get(999));
+
+		 //you can also verify using an argument matcher
+		 verify(mockedList).get(anyInt());
 	}
 }
